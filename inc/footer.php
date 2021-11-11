710667c180ff408dad938eadf10e1cd645e9f9cb
newtonoluoch7 <newtonoluoch7@gmail.com>
2021-11-11 23:49:29 +0100

    big edit

---
+++ inc/footer.php
@@ -0,0 +1,70 @@
+<footer>
+    <div class="container"> 
+      
+      <!-- ABOUT Location -->
+      <div class="col-md-3">
+        <div class="about-footer"> <img class="margin-bottom-30" src="images/RTgrey.svg" style="height: 45px;" alt="" >
+          <p><i class="icon-pointer"></i> Nairobi Parklands <br>UoN School of Law.</p>
+          <p><i class="icon-call-end"></i> +254 759 413883</p>
+          <p><i class="icon-envelope"></i> info@theroundtable.co.ke</p>
+        </div>
+      </div>
+      
+      <!-- HELPFUL LINKS -->
+      <div class="col-md-3">
+        <h6>HELPFUL LINKS</h6>
+        <ul class="link">
+          <li><a href="index.html"> Home</a></li>
+          <li><a href="about-us_01.html"> About</a></li>
+          <li><a href="shop_04.html"> Shop</a></li>
+          <li><a href="contact.html"> Contact</a></li>
+          <li><a href="blog-list_02.html"> Blog</a></li>
+        </ul>
+      </div>
+      
+      <!-- SHOP -->
+      <div class="col-md-3">
+        <h6>SHOP</h6>
+        <ul class="link">
+          <li><a href="index.html">Book 1</a></li>
+          <li><a href="about-us_01.html">Book 2</a></li>
+          <li><a href="shop_04.html">Book 3</a></li>
+          <li><a href="contact.html">Book 4</a></li>
+          <li><a href="blog-list_02.html">Book 5</a></li>
+        </ul>
+      </div>
+      
+      <!-- MY ACCOUNT -->
+      <div class="col-md-3">
+        <h6>ABOUT US</h6>
+        <ul class="link">
+         <li> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit, officiis nisi error deleniti debitis, quae voluptate 
+          nihil explicabo magnam, possimus mollitia sed aperiam accusantium beatae adipisci deserunt temporibus quibusdam unde.</li>
+        </ul>
+      </div>
+      
+      <!-- Rights -->
+      
+      <div class="rights">
+        <p>&copy; 2021 The Roundtable Magazine | All right reserved | Designed with &hearts; by NOJO LLP </p>
+        <div class="scroll"> <a href="#wrap" class="go-up"><i class="lnr lnr-arrow-up"></i></a> </div>
+      </div>
+    </div>
+  </footer>
+  
+  <!--======= RIGHTS =========--> 
+  
+</div>
+<script src="js/jquery-1.11.3.min.js"></script> 
+<script src="js/bootstrap.min.js"></script> 
+<script src="js/own-menu.js"></script> 
+<script src="js/jquery.lighter.js"></script> 
+<script src="js/owl.carousel.min.js"></script> 
+
+<!-- SLIDER REVOLUTION 4.x SCRIPTS  --> 
+<script type="text/javascript" src="rs-plugin/js/jquery.tp.t.min.js"></script> 
+<script type="text/javascript" src="rs-plugin/js/jquery.tp.min.js"></script> 
+<script src="js/main.js"></script> 
+<script src="js/main.js"></script>
+</body>
+</html>
---
+++ inc/header.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="">
+<meta name="author" content="M_Adnan">
+<title>The Roundtable - Magazine</title>
+
+<!-- SLIDER REVOLUTION 4.x CSS SETTINGS -->
+<link rel="stylesheet" type="text/css" href="rs-plugin/css/settings.css" media="screen" />
+
+<!-- Bootstrap Core CSS -->
+<link href="css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Custom CSS -->
+<link href="css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link href="css/ionicons.min.css" rel="stylesheet">
+<link href="css/main.css" rel="stylesheet">
+<link href="css/style.css" rel="stylesheet">
+<link href="css/responsive.css" rel="stylesheet">
+
+<!-- JavaScripts -->
+<script src="js/modernizr.js"></script>
+
+<!-- Online Fonts -->
+<link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Playfair+Display:400,700,900' rel='stylesheet' type='text/css'>
+
+<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+
+</head>
+<body>
+
+<!-- LOADER -->
+<div id="loader">
+  <div class="position-center-center">
+    <div class="ldr"></div>
+  </div>
+</div>
+
+<!-- Wrap -->
+<div id="wrap"> 
+  
+  <!-- header -->
+  <header>
+    <div class="sticky">
+      <div class="container"> 
+        
+        <!-- Logo -->
+        <div class="logo"> <a href="index.html"><img class="img-responsive" src="images/RT.png" style="height: 50px;" alt="logo" ></a> </div>
+        <nav class="navbar ownmenu">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#nav-open-btn" aria-expanded="false"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"><i class="fa fa-navicon"></i></span> </button>
+          </div>
+          
+          <!-- NAV -->
+          <div class="collapse navbar-collapse" id="nav-open-btn">
+            <ul class="nav">
+              <li> <a href="index.html">Home </a> </li>
+              <li> <a href="about-us_01.html">About </a> </li> 
+           <li> <a href="shop_04.html">Shop</a></li>
+           <li> <a href="contact.html">Contact</a></li>
+           <li><a href="blog-list_02.html">Blog</a></li>
+            </ul>
+          </div>
+          
+          <!-- Nav Right -->
+          <div class="nav-right">
+            <ul class="navbar-right">
+              
+              <!-- USER INFO -->
+              <li class="dropdown user-acc"> <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" ><i class="icon-user"></i> </a>
+                <ul class="dropdown-menu">
+                  <li>
+                    <h6>HELLO! Lenah John</h6>
+                  </li>
+                  <li><a href="#">MY CART</a></li>
+                  <li><a href="#">ACCOUNT INFO</a></li>
+                  <li><a href="#">LOG OUT</a></li>
+                </ul>
+              </li>
+              
+              <!-- USER BASKET -->
+              <li class="dropdown user-basket"> <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="true"><i class="icon-basket-loaded"></i> </a>
+                <ul class="dropdown-menu">
+                  <li>
+                    <div class="media-left">
+                      <div class="cart-img"> <a href="#"> <img class="media-object img-responsive" src="images/cart-img-1.jpg" alt="..."> </a> </div>
+                    </div>
+                    <div class="media-body">
+                      <h6 class="media-heading">WOOD CHAIR</h6>
+                      <span class="price">129.00 USD</span> <span class="qty">QTY: 01</span> </div>
+                  </li>
+                  <li>
+                    <div class="media-left">
+                      <div class="cart-img"> <a href="#"> <img class="media-object img-responsive" src="images/cart-img-2.jpg" alt="..."> </a> </div>
+                    </div>
+                    <div class="media-body">
+                      <h6 class="media-heading">WOOD STOOL</h6>
+                      <span class="price">129.00 USD</span> <span class="qty">QTY: 01</span> </div>
+                  </li>
+                  <li>
+                    <h5 class="text-center">SUBTOTAL: 258.00 USD</h5>
+                  </li>
+                  <li class="margin-0">
+                    <div class="row">
+                      <div class="col-xs-6"> <a href="shopping-cart.html" class="btn">VIEW CART</a></div>
+                      <div class="col-xs-6 "> <a href="checkout.html" class="btn">CHECK OUT</a></div>
+                    </div>
+                  </li>
+                </ul>
+              </li>
+              
+              <!-- SEARCH BAR -->
+              <li class="dropdown"> <a href="javascript:void(0);" class="search-open"><i class=" icon-magnifier"></i></a>
+                <div class="search-inside animated bounceInUp"> <i class="icon-close search-close"></i>
+                  <div class="search-overlay"></div>
+                  <div class="position-center-center">
+                    <div class="search">
+                      <form>
+                        <input type="search" placeholder="Search Shop">
+                        <button type="submit"><i class="icon-check"></i></button>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </div>
+  </header>
